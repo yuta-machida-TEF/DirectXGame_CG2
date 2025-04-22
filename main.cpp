8ecf12586da529e5014ff6b758032365a40304e0
yuta-machida-TEF <k024g0029@g.neec.ac.jp>
2025-04-22 11:20:32 +0900

    CG2_0-4完了

--- main.cpp
+++ main.cpp
@@ -1,5 +1,52 @@
 #include<Windows.h>
-#include<cstdint>
+#include<string>
+#include<format>
+
+std::wstring ConverString(const std::string& str)
+{
+	if (str.empty())
+	{
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0,
+		reinterpret_cast<const char*>(&str[0]), static_cast<int>
+		(str.size()), NULL, 0);
+	if (sizeNeeded == 0)
+	{
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]),
+	static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+
+std::string ConverString(const std::wstring& str)
+{
+	if (str.empty())
+	{
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, 
+		str.data(), static_cast<int>(str.size()),NULL,0, NULL, NULL);
+	if (sizeNeeded == 0)
+	{
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>
+		(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+
+}
+
+void Log(const std::string& message)
+{
+	OutputDebugStringA(message.c_str());
+}
+
 
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
@@ -23,7 +70,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
 //WIndowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
-	
+
 	WNDCLASS wc{};
 	//ウィンドウブロシージャ
 	wc.lpfnWndProc = WindowProc;
@@ -32,7 +79,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//インスタンスハンドル
 	wc.hInstance = GetModuleHandle(nullptr);//スペル間違いを
 	//カーソル
-	wc.hCursor = LoadCursor(nullptr,IDC_ARROW);
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
 
 	//ウィンドウクラスを登録する
 	RegisterClass(&wc);
@@ -70,18 +117,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//ウィンドウのxボタンが押されるまでループ
 	while (msg.message != WM_QUIT) {
 		//Windowにメッセージが来てたら最優先で処理させる
-		if (PeekMessage(&msg,NULL,0,0,PM_REMOVE))
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 		{
 			TranslateMessage(&msg);
-			DispatchMessage(&msg);		
-		}
-		else 
+			DispatchMessage(&msg);
+		} else
 		{
 			//ゲーム処理.
+			//Log(std::format("enemyHP:{},textruePath:{}\n", enemyHp, text))
 		}
 	}
 
+	Log(ConverString(std::format(L"WSTRING{}\n", L"abc")));
 
 	return 0;
 }
-
