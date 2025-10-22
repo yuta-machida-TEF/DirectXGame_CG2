1c4101e50cf317a300dd9908b93aaa0f88311936
yuta-machida-TEF <k024g0029@g.neec.ac.jp>
2025-10-22 11:52:50 +0900

    CG3_01-00完了

--- main.cpp
+++ main.cpp
@@ -1424,6 +1424,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		{
 			Matrix4x4 worldMatrix =
 				MakeAffineMatrix(transforms[index].scale, transforms[index].rotate, transforms[index].translate);
+			Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, viewProjectionMatrix);
 			instancingData[index].WVP = worldViewProjectionMatrix;
 			instancingData[index].World = worldMatrix;
 
@@ -1470,6 +1471,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		//指定した深度で画面全体をクリアする
 		commandList->ClearDepthStencilView(dsvHandle, D3D12_CLEAR_FLAG_DEPTH, 1.0f, 0, 0, nullptr);
 
+
 		commandList->RSSetViewports(1, &viewport);//Viewportを設定
 		commandList->RSSetScissorRects(1, &scissorRect);//Scirssorを設定
 		//RootSignatrueを設定。PSPに設定しているけど別途設定が必要
@@ -1477,11 +1479,18 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		commandList->SetPipelineState(graphicsPipelineState);//PSOを設定
 		commandList->IASetVertexBuffers(0, 1, &vertexBufferView);//VBVを設定
 		//形状を設定。PSOに設定しているものとはまた別。同じものを設定すると考えておけば良い
-		//commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+		commandList->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 		//マテリアルCBufferの場所を設定
 		commandList->SetGraphicsRootConstantBufferView(0, materialResource->GetGPUVirtualAddress());
 		//commandList->SetGraphicsRootConstantBufferView(1, wvpResource->GetGPUVirtualAddress());//これをいれないと描画ができない
 		commandList->SetGraphicsRootDescriptorTable(2, textureSrvHandleGPU);
+
+		//他の設定諸々
+		//instancing用のDataを読むためにStructureBufferのSRVを設定する
+		commandList->SetGraphicsRootDescriptorTable(1, instancingSrvHandleGPU);
+		//他の設定諸々
+		//描画!6頂点の板ポリゴンを、kNumInstance(今回は10)だけInstanceを描画を行う
+		commandList->DrawInstanced(UINT(modelData.vertices.size()), kNumInstance, 0, 0);
 
 		//transform.rotate.y += 0.03f;
 
@@ -1507,12 +1516,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		//TransitionBarrierを振る
 		commandList->ResourceBarrier(1, &barrier);
 
-		//他の設定諸々
-	    //instancing用のDetaを読むためにStructureBufferのSRVを設定する
-		commandList->SetComputeRootDescriptorTable(1, instancingSrvHandleGPU);
-		//他の設定諸々
-		//描画! 6頂点の板ポリゴンを,kNumInstance(今回は10)だけInstance描画を行う
-		//commandList->DrawInstanced(UINT(modelData.vertices.size()), kNumInstance, 0, 0);
+		
 
 		//コマンドリストの内容を確定させる。すべてのコマンドをつんでからCloseすること
 		hr = commandList->Close();
