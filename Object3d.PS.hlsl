181c87f547fdb0831b5cb9b129896dbdd41d390a
yuta-machida-TEF <k024g0029@g.neec.ac.jp>
2026-01-14 12:02:16 +0900

    05-04完了

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -33,8 +33,11 @@ PixelShaderOutput main(VertexShaderOutput input)
     
     if(gMaterial.enableLighting != 0)
     {
-        float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
-        output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;  
+          //half lambert
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
+        output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
+        
     }
     else
     {
